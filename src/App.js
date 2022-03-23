da17b2cd14c5af77e0285e57c1ff0f92f0231df2
Marika Stenberg <stenberg.marika@outlook.com>
2022-03-23 19:57:09 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -8,45 +8,63 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 import DateFnsUtils from '@date-io/date-fns';
 import {DatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
+
 
 function App() {
   const [todo, setTodo] = useState({desc: '', date: '', priority: ''});
   const [todos, setTodos] = useState([]);
 
+  const [value, setValue] = useState('one');
+
+  const handleChanged = (event, value) => {
+    setValue(value);
+  };
+
   const addTodo = (event) => {
     setTodos([...todos, todo]);
     setTodo({desc: '', date: '', priority: ''});
-  }
+  };
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+  } ;
 
   const columns = [
     { field: 'desc', sortable: true, filter: true },
     { field: 'date', sortable: true, filter: true },
     { field: 'priority', sortable: true, filter: true },
-  ]
+  ];
 
   return (
     <div className="App">
-      <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker label="Date" name="date" value={todo.date} onChange={pv => setTodo({...todo, date: pv})} animateYearScrolling/>
-      </MuiPickersUtilsProvider>
-        <TextField label="Description" variant="standard" name="desc" value={todo.desc} onChange={inputChanged}/>
-        <TextField label="Priority" variant='standard' name="priority" value={todo.priority} onChange={inputChanged}/>
-        <Button onClick={addTodo} variant="contained">Add</Button>
-      </Stack>
-      <div className="ag-theme-material" style={{height: 400, width: 600, margin: 'auto'}}>
-        <AgGridReact
-          rowData={todos}
-          columnDefs={columns}>
-        </AgGridReact>
-      </div>
+      <Tabs value={value} onChange={handleChanged}>
+        <Tab value="one" label="Home"></Tab>
+        <Tab value="two" label="TODOS"></Tab>
+      </Tabs>
+      {value === 'one' && <div>
+        <p>Tervetuloa testailemaan välilehtiä ja muistiota</p>
+        </div>}
+      {value === 'two' && <div>
+        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker label="Date" name="date" value={todo.date} onChange={pv => setTodo({...todo, date: pv})} animateYearScrolling/>
+          </MuiPickersUtilsProvider>
+          <TextField label="Description" variant="standard" name="desc" value={todo.desc} onChange={inputChanged}/>
+          <TextField label="Priority" variant='standard' name="priority" value={todo.priority} onChange={inputChanged}/>
+          <Button onClick={addTodo} variant="contained">Add</Button>
+        </Stack>
+       <div className="ag-theme-material" style={{height: 400, width: 600, margin: 'auto'}}>
+          <AgGridReact
+            rowData={todos}
+            columnDefs={columns}>
+          </AgGridReact>
+        </div>
+      </div>}
     </div>
   );
 }
